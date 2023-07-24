dc482e39ea4099910d7e67f383d766d8ac245c2b
Artem Shlagin <artem@shlagin.com>
2023-07-24 19:39:45 +0700

    Initial commit

---
+++ apps/multiple-chunks-bug-demo/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'multiple-chunks-bug-demo',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/multiple-chunks-bug-demo',
+};
